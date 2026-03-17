a32b00dfe171da6d6360927e8c0c0ad759a342c2
njbk1209 <njbk1996@gmail.com>
2026-03-17 14:29:27 -0400

    Corrección de importación de vercel speed

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import './index.css'
 import App from './App.jsx'
 import { CartProvider } from './context/CartContext.jsx'
 import { Toaster } from 'react-hot-toast';
-import { SpeedInsights } from '@vercel/speed-insights/next';
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
